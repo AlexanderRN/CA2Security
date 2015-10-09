2fcee67affcb1ded93d21fb99a5441344ec67b72
AlexanderRN <AlexanderRNielsen@hotmail.dk>
2015-10-09 12:00:59 +0200

    Added the methods back into the Test after problems. Working fine, lack of dummy data makes it difficult to test more.

--- test/dk/cphbusiness/text/FacadeTest.java
+++ test/dk/cphbusiness/text/FacadeTest.java
@@ -1,8 +1,51 @@
 package dk.cphbusiness.text;
 
+import dk.cphbusiness.entity.Person;
+import dk.cphbusiness.exceptions.PersonNotFoundException;
+import dk.cphbusiness.facade.PersonFacade;
+import java.io.IOException;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import junit.framework.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class FacadeTest {
+
+    static EntityManagerFactory emf;
+    static PersonFacade f;
     
-    public FacadeTest() {
+    @BeforeClass
+    public static void setUpClass() {
+        emf = Persistence.createEntityManagerFactory("CA2-Security-TestPU");
+        f = new PersonFacade(emf);
+    }
+
+    @Test
+    public void testGetPerson() throws IOException, PersonNotFoundException {  
+        int id = 1;
+        Person p = f.getPerson(id);
+        
+        Assert.assertEquals("Bo", p.getFirstName());
     }
     
+    @Test
+    public void testCreatePerson() throws IOException, PersonNotFoundException {
+        Person p = new Person();
+        p.setFirstName("TestCreate");
+        p.setLastName("TestCreate");
+
+        f.createPerson(p);
+        Person p1 = f.getPerson(4);
+        Assert.assertEquals("TestCreate", p1.getFirstName());
+    }
+    
+    @Test
+    public void testDeletePerson() throws IOException, PersonNotFoundException{
+        f.removePerson(3);
+        
+        Person p = f.getPerson(3);
+        Assert.assertEquals(null, p);
+    }
+
 }
